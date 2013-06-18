4e46ec6c8a85672099ca090d19b9c485b8c22e6a
Jon Kessler <jon.kessler@appfolio.com>
2013-06-18 12:05:54 -0700

    update metadata

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name             'jonkessler-myface'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'Jon Kessler'
+maintainer_email 'jwkpiano1@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures jonkessler-myface'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
